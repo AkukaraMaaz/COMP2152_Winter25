e84b4a3346ed9d430849c7dcd66205aa776b02cd
AkukaraMaaz <mdmaazakukara@gmail.com>
2025-01-27 22:36:07 -0500

    Hello World

---
+++ lab01.py
@@ -0,0 +1,17 @@
+# Sample Coding Questions 01 Week 01
+# Maaz Akukara
+
+# Defining Variables
+array_variable = [1, 4, 7, 9]
+
+# Order of Operations
+a, b, c, d = 1, 2, 3, 4
+e = (a - ((b ** c) // d) + (a % c))
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+print("Now showing the shop items filtered by age: {}".format(userAge))
